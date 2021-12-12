69bbebe7c752d5e6eb221b4358b9810c9459f831
MaxChernyshev <maksim.chernyshev@gmail.com>
2021-12-12 22:37:09 +0200

    maintance mode

--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -2,39 +2,69 @@
 
 @section('content')
 
-    <div class="container-fluid px-0 ">
+    <div class="container-fluid px-0">
         <div class="row">
             <div class="col-12">
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner bg-info" role="listbox">
-                        <div class="carousel-item active">
-                            <div class="d-flex align-items-center justify-content-center min-vh-100">
-                                <img src="{{ asset('/img/carousel/0C6A0030.jpg') }}" class="d-block w-100 img-fluid" alt="...">
-                            </div>
-                        </div>
-{{--                        <div class="carousel-item">--}}
-{{--                            <div class="d-flex align-items-center justify-content-center min-vh-100">--}}
-{{--                                <img src="{{ asset('/img/carousel/0S5A6511.jpg') }}" class="d-block w-100 img-fluid" alt="...">--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="carousel-item">--}}
-{{--                            <div class="d-flex align-items-center justify-content-center min-vh-100">--}}
-{{--                                <img src="{{ asset('/img/carousel/35ph_007_900x600.jpg') }}" class="d-block w-100 img-fluid" alt="...">--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-                    </div>
-{{--                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">--}}
-{{--                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
-{{--                        <span class="sr-only">Previous</span>--}}
-{{--                    </a>--}}
-{{--                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">--}}
-{{--                        <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
-{{--                        <span class="sr-only">Next</span>--}}
-{{--                    </a>--}}
-                </div>
+                <img src="{{ asset('/img/carousel/0C6A0030.jpg') }}" style="object-fit: cover" class="d-block w-100 img-fluid" alt="...">
             </div>
         </div>
     </div>
+
+{{--    <div class="container-fluid px-0 ">--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-12">--}}
+                {{--                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">--}}
+                {{--                    <div class="carousel-inner bg-info" role="listbox">--}}
+                {{--                        <div class="carousel-item active">--}}
+                {{--                            <div class="d-flex align-items-center justify-content-center min-vh-100">--}}
+                {{--                                <img src="{{ asset('/img/carousel/0C6A0030.jpg') }}" class="d-block w-100 img-fluid" alt="...">--}}
+                {{--                            </div>--}}
+                {{--                        </div>--}}
+                {{--                        <div class="carousel-item">--}}
+                {{--                            <div class="d-flex align-items-center justify-content-center min-vh-100">--}}
+                {{--                                <img src="{{ asset('/img/carousel/0S5A6511.jpg') }}" style="object-fit: cover" class="d-block w-100 img-fluid" alt="...">--}}
+                {{--                            </div>--}}
+                {{--                        </div>--}}
+                {{--                        <div class="carousel-item">--}}
+                {{--                            <div class="d-flex align-items-center justify-content-center min-vh-100">--}}
+                {{--                                <img src="{{ asset('/img/carousel/35ph_007_900x600.jpg') }}"  class="d-block w-100 img-fluid" alt="...">--}}
+                {{--                            </div>--}}
+                {{--                        </div>--}}
+                {{--                    </div>--}}
+                {{--                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">--}}
+                {{--                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
+                {{--                        <span class="sr-only">Previous</span>--}}
+                {{--                    </a>--}}
+                {{--                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">--}}
+                {{--                        <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
+                {{--                        <span class="sr-only">Next</span>--}}
+                {{--                    </a>--}}
+                {{--                </div>--}}
+
+{{--                <div id="carouselExampleControls" class="carousel slide" style="height: 100vh; width: 100vw;" data-ride="carousel">--}}
+{{--                    <div class="carousel-inner">--}}
+{{--                        <div class="carousel-item active">--}}
+{{--                            <img class="d-block " src="{{ asset('/img/carousel/0C6A0030.jpg') }}" style="width: 100%; margin-top: -30%" alt="">--}}
+{{--                        </div>--}}
+{{--                        <div class="carousel-item">--}}
+{{--                            <img class="d-block " src="{{ asset('/img/carousel/0S5A6511.jpg') }}" style="width: 100%; margin-top: -30%" alt="">--}}
+{{--                        </div>--}}
+{{--                        <div class="carousel-item">--}}
+{{--                            <img class="d-block " src="{{ asset('/img/carousel/35ph_007_900x600.jpg') }}" style="width: 100%; margin-top: -30%" alt="">--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">--}}
+{{--                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
+{{--                        <span class="sr-only"></span>--}}
+{{--                    </a>--}}
+{{--                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">--}}
+{{--                        <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
+{{--                        <span class="sr-only"></span>--}}
+{{--                    </a>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
 
 
